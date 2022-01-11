a12f44a49b1306467bc41ccdeb6e6feecae03c98
jvargas <jhonny.knaak@protonmail.com>
2022-01-11 17:33:32 +0000

    Failing test to deal with String type.

--- lib/src/test/java/EverVault/WhenEncryptingDifferentTypesOfDataTests.java
+++ lib/src/test/java/EverVault/WhenEncryptingDifferentTypesOfDataTests.java
@@ -2,17 +2,20 @@ package EverVault;
 
 import EverVault.Contracts.IDataHandler;
 import EverVault.Contracts.IProvideEncryption;
+import org.bouncycastle.crypto.InvalidCipherTextException;
 import org.junit.jupiter.api.Test;
 
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class WhenEncryptingDifferentTypesOfDataTests {
     @Test
-    void handlesStringCorrectly() throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException {
+    void handlesStringCorrectly() throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, InvalidCipherTextException {
         var setup = new EncryptSetup();
         var encryptionProvider = mock(IProvideEncryption.class);
 
@@ -21,7 +24,10 @@ public class WhenEncryptingDifferentTypesOfDataTests {
         });
 
         var someString = "Foo";
+        var someReturn = "Bar";
 
-        encryptObjectService.Encrypt(setup.keyPair.getPublic().getEncoded(), setup.sharedKey, someString);
+        when(encryptionProvider.encryptData(any(), any(), any(), any())).thenReturn(someReturn);
+
+        assert encryptObjectService.Encrypt(setup.keyPair.getPublic().getEncoded(), setup.sharedKey, someString).equals(someReturn);
     }
 }
