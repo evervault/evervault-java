39adf0e5e6e9ff38d1b3d3c5f61560c46b4eeac6
Jhonny Knaak de Vargas <jhonny.knaak@protonmail.com>
2022-01-18 17:21:48 +0000

    Working on resource control tests.

--- lib/src/main/java/EverVault/Services/ResourceControl.java
+++ lib/src/main/java/EverVault/Services/ResourceControl.java
@@ -1,34 +1,29 @@
 package EverVault.Services;
 
 import java.util.Objects;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ResourceControl {
     public static final int COUNT_LIMIT = 3;
     public static final long CB_TIME_TO_FREE_MILLISECS = 30000;
-    private final TimerTask timerTask;
+    private final Runnable unlockTask;
 
     private int counter;
-    private final Timer timer;
     private boolean blocked;
     private final int countLimit;
-    private long timeToFreeMilliseconds;
+    private final long timeToFreeMilliseconds;
 
     public boolean getBlocked() {
         return blocked;
     }
 
-    public ResourceControl(int countLimit, long timeToFreeMilliseconds, TimerTask timerTask) {
+    public ResourceControl(int countLimit, long timeToFreeMilliseconds, Runnable timerTask) {
         this.timeToFreeMilliseconds = timeToFreeMilliseconds;
-        this.timer = new Timer();
         this.countLimit = countLimit;
-        this.timerTask = Objects.requireNonNullElseGet(timerTask, () -> new TimerTask() {
-            @Override
-            public void run() {
-                blocked = false;
-                counter = 0;
-            }
+        this.unlockTask = Objects.requireNonNullElseGet(timerTask, () -> () -> {
+            blocked = false;
+            counter = 0;
         });
     }
 
@@ -38,7 +33,10 @@ public class ResourceControl {
 
     public void timeOutOccurred() {
         if (counter == countLimit) {
-            this.timer.schedule(timerTask, timeToFreeMilliseconds);
+            blocked = true;
+
+            var executor = Executors.newSingleThreadScheduledExecutor();
+            executor.schedule(unlockTask, timeToFreeMilliseconds, TimeUnit.MILLISECONDS);
         } else {
             counter++;
         }
