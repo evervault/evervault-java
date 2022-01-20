26aff36e2a5b9aa6154362dfd1e5a95ad8919fc8
Jhonny Knaak de Vargas <jhonny.knaak@protonmail.com>
2022-01-20 13:23:23 +0000

    Adding SDKMAN and gradle to the build phase

--- install_dependencies.bash
+++ install_dependencies.bash
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt search openjdk
+sudo apt install openjdk-17-jdk
+
+# install SDKMAN, recommended by gradle website
+curl -s "https://get.sdkman.io" | bash
+
+sdk install gradle 7.3.3
