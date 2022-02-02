b5c84aab272b2ec2d71ab57e9af8f8befb94bc68
Eoin Boylan <jb.eoin@gmail.com>
2022-02-02 16:55:03 +0000

    Remove HashMap in curve enum

--- lib/src/main/java/evervault/utils/EcdhCurve.java
+++ lib/src/main/java/evervault/utils/EcdhCurve.java
@@ -1,8 +1,5 @@
 package evervault.utils;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public enum EcdhCurve {
     SECP256K1("secp256k1"),
     SECP256R1("secp256r1");
@@ -13,15 +10,4 @@ public enum EcdhCurve {
         this.curveId = curveId;
     }
 
-    public String getEcdhCurve() {
-        return this.curveId;
-    }
-
-    static final Map<String, EcdhCurve> curveMap = new HashMap<String, EcdhCurve>();
-
-    static {
-        for (EcdhCurve curve: EcdhCurve.values()) {
-            curveMap.put(curve.toString(), curve);
-        }
-    }
 }
