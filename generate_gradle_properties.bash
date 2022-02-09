96b35c5695ed242f6ccfcd6808d84a807286e55e
Jhonny Knaak de Vargas <jhonny.knaak@protonmail.com>
2022-02-09 16:40:51 +0000

    Fixing script.

--- generate_gradle_properties.bash
+++ generate_gradle_properties.bash
@@ -3,7 +3,7 @@
 # https://central.sonatype.org/publish/publish-gradle/#metadata-definition-and-upload
 
 # create gpg key file
-echo ${GPG_KEY_FILE} | base64 -d > result > ${HOME}/gpg_key_file.gpg
+echo ${GPG_KEY_FILE} | base64 -d > ${HOME}/gpg_key_file.gpg
 
 echo "signing.keyId=${GPG_KEY_ID}" >> ${HOME}/gradle.properties
 echo "signing.password=${GPG_KEY_PASSWORD}" >> ${HOME}/gradle.properties
