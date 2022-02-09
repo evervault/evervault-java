112cb904c9324dd7078225704e31a41939923ce3
Jhonny Knaak de Vargas <jhonny.knaak@protonmail.com>
2022-02-09 16:39:43 +0000

    Using secrets for publishing.

--- generate_gradle_properties.bash
+++ generate_gradle_properties.bash
@@ -2,8 +2,11 @@
 
 # https://central.sonatype.org/publish/publish-gradle/#metadata-definition-and-upload
 
-echo "signing.keyId=test" >> ${HOME}/gradle.properties
-echo "signing.password=${2}" >> ${HOME}/gradle.properties
-echo "signing.secretKeyRingFile=${3}" >> ${HOME}/gradle.properties
+# create gpg key file
+echo ${GPG_KEY_FILE} | base64 -d > result > ${HOME}/gpg_key_file.gpg
+
+echo "signing.keyId=${GPG_KEY_ID}" >> ${HOME}/gradle.properties
+echo "signing.password=${GPG_KEY_PASSWORD}" >> ${HOME}/gradle.properties
+echo "signing.secretKeyRingFile=${HOME}/gpg_key_file.gpg" >> ${HOME}/gradle.properties
 echo "ossrhUsername=${SONATYPE_USERNAME}" >> ${HOME}/gradle.properties
 echo "ossrhPassword=${SONATYPE_PASSWORD}" >> ${HOME}/gradle.properties
