2406aacf3c7b13dc0267431248ec08447d385024
Jhonny Knaak de Vargas <jhonny.knaak@protonmail.com>
2022-02-10 15:20:01 +0000

    moving gradle.properties to ~/.gradle/

--- generate_gradle_properties.bash
+++ generate_gradle_properties.bash
@@ -5,8 +5,8 @@
 # create gpg key file
 echo ${GPG_KEY_FILE} | base64 -d > ${HOME}/gpg_key_file.gpg
 
-echo "signing.keyId=${GPG_KEY_ID}" >> ${HOME}/gradle.properties
-echo "signing.password=${GPG_KEY_PASSWORD}" >> ${HOME}/gradle.properties
-echo "signing.secretKeyRingFile=${HOME}/gpg_key_file.gpg" >> ${HOME}/gradle.properties
-echo "ossrhUsername=${SONATYPE_USERNAME}" >> ${HOME}/gradle.properties
-echo "ossrhPassword=${SONATYPE_PASSWORD}" >> ${HOME}/gradle.properties
+echo "signing.keyId=${GPG_KEY_ID}" >> ${HOME}/.gradle/gradle.properties
+echo "signing.password=${GPG_KEY_PASSWORD}" >> ${HOME}/.gradle/gradle.properties
+echo "signing.secretKeyRingFile=${HOME}/gpg_key_file.gpg" >> ${HOME}/.gradle/gradle.properties
+echo "ossrhUsername=${SONATYPE_USERNAME}" >> ${HOME}/.gradle/gradle.properties
+echo "ossrhPassword=${SONATYPE_PASSWORD}" >> ${HOME}/.gradle/gradle.properties
