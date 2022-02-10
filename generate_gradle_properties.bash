212f73928ee8c0fd3458459a190f2adaee20d226
Jhonny Knaak de Vargas <jhonny.knaak@protonmail.com>
2022-02-10 17:14:35 +0000

    Trying to export gpg file in the correct way

--- generate_gradle_properties.bash
+++ generate_gradle_properties.bash
@@ -8,12 +8,18 @@ echo ${GPG_KEY_FILE} > ${HOME}/gpg_key_file.key
 # importing key back gpg, so we can export it back to the expected format
 gpg --import ${HOME}/gpg_key_file.key
 
+echo "PGP file imported"
+
 if [ ! -d ${HOME}/.gradle ]; then
   mkdir -p ${HOME}/.gradle
 fi
 
+gpg --export-secret-keys ${GPG_KEY_ID} > gpg_key_file.gpg
+
+echo "key exported in expected format" 
+
 echo "signing.keyId=${GPG_KEY_ID}" >> ${HOME}/.gradle/gradle.properties
 echo "signing.password=${GPG_KEY_PASSWORD}" >> ${HOME}/.gradle/gradle.properties
-echo "signing.secretKeyRingFile=${HOME}/gpg_key_file.key" >> ${HOME}/.gradle/gradle.properties
+echo "signing.secretKeyRingFile=${HOME}/gpg_key_file.gpg" >> ${HOME}/.gradle/gradle.properties
 echo "ossrhUsername=${SONATYPE_USERNAME}" >> ${HOME}/.gradle/gradle.properties
 echo "ossrhPassword=${SONATYPE_PASSWORD}" >> ${HOME}/.gradle/gradle.properties
