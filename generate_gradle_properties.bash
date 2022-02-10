56ba11d7a0d0a8f93db3d5fada2a3eaf20bbe37c
Jhonny Knaak de Vargas <jhonny.knaak@protonmail.com>
2022-02-10 16:35:02 +0000

    Changing format of the key file

--- generate_gradle_properties.bash
+++ generate_gradle_properties.bash
@@ -3,7 +3,7 @@
 # https://central.sonatype.org/publish/publish-gradle/#metadata-definition-and-upload
 
 # create gpg key file
-echo ${GPG_KEY_FILE} | base64 -d > ${HOME}/gpg_key_file.gpg
+echo ${GPG_KEY_FILE} > ${HOME}/gpg_key_file.key
 
 if [ ! -d ${HOME}/.gradle ]; then
   mkdir -p ${HOME}/.gradle
@@ -11,6 +11,6 @@ fi
 
 echo "signing.keyId=${GPG_KEY_ID}" >> ${HOME}/.gradle/gradle.properties
 echo "signing.password=${GPG_KEY_PASSWORD}" >> ${HOME}/.gradle/gradle.properties
-echo "signing.secretKeyRingFile=${HOME}/gpg_key_file.gpg" >> ${HOME}/.gradle/gradle.properties
+echo "signing.secretKeyRingFile=${HOME}/gpg_key_file.key" >> ${HOME}/.gradle/gradle.properties
 echo "ossrhUsername=${SONATYPE_USERNAME}" >> ${HOME}/.gradle/gradle.properties
 echo "ossrhPassword=${SONATYPE_PASSWORD}" >> ${HOME}/.gradle/gradle.properties
